db218e52a2194aa79ca0f3efc5e0b34ad0a50bf2
LindaBouchibane <lindabouchibane@gmail.com>
2024-05-24 09:26:57 -0500

    Déplacement des fichiers dans un nouveau dossier, sauf hb_overview.png, hb_mld.md, hb_dico.md

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:azerty123@localhost/postgres'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
