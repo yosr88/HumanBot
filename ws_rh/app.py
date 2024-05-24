8d9e34e50c78ec7b8f96c5d2197712c7ff78fb75
ihebWer <158372804+ihebWer@users.noreply.github.com>
2024-05-24 16:29:03 +0200

    ws_rh -Iheb

--- ws_rh/app.py
+++ ws_rh/app.py
@@ -1,24 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from config import Config 
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
-class Employee(db.Model):
-    __tablename__ = 'EMPLOYE'
-    code_employe = db.Column(db.Integer, primary_key=True)
-    nom_employe = db.Column(db.String(50), nullable=False)
-    prenom_employe = db.Column(db.String(50), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    date_arrivee = db.Column(db.Date, nullable=False)
-    date_depart = db.Column(db.Date)
+from models import Employe
 
-@app.route('/')
+@app.route('/employe')
 def index():
-    employees = Employee.query.filter_by(date_depart=None).order_by(Employee.date_arrivee.desc()).all()
+    employees = Employe.query.filter_by(date_depart=None).order_by(Employe.date_arrivee.desc()).all()
     return render_template('index.html', employees=employees)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Crée les tables si elles n'existent pas
     app.run(debug=True)
